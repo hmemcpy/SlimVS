f3897e108f3290edfa8379b06ee3c2796af10f66
Igal Tabachnik <hmemcpy@gmail.com>
2015-04-04 10:33:00 +0300

    Adding a small script to create pkgundef

---
+++ Create-PkgUndefFile.ps1
@@ -0,0 +1,24 @@
+﻿function Create-PkgUndefFile {
+    param([string]$packagesFile)
+
+    $fileName = ".\devenv.pkgundef"
+    ";SlimVS by Igal Tabachnik" | Out-File $fileName
+    Get-Content $packagesFile | % {
+        [string]$line = $_
+        
+        if ($line.StartsWith(";Id:")) {
+            if ($line -match "\{[0-9A-F]{8}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{4}-[0-9A-F]{12}\}") {
+                $line | Out-File $fileName -Append
+                "[`$RootKey$`\Packages\{0}]" -f $Matches[0] | Out-File $fileName -Append;
+            }
+            
+        }
+        else {
+            $line | Out-File $fileName -Append
+        }
+    }
+
+
+}
+
+Create-PkgUndefFile .\packages.txt
